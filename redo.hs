37408f9baf912e2c52251a81489b7209b8d40050
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-05-25 15:37:51 +0200

    recreate file only on success

--- redo.hs
+++ redo.hs
@@ -1,5 +1,8 @@
+import System.Directory (renameFile, removeFile)
 import System.Process (shell, createProcess, waitForProcess)
 import System.Environment (getArgs)
+import System.Exit (ExitCode(..))
+import System.IO (stderr, hPutStrLn)
 
 main :: IO ()
 main = do
@@ -8,6 +11,10 @@ main = do
 
 redo :: String -> IO ()
 redo target = do
-  (_,_,_,ph) <- createProcess $ shell $ "sh " ++ target ++ ".do"
-  _ <- waitForProcess ph
-  return ()
+  let tmp = target ++ "---redoing"
+  (_,_,_,ph) <- createProcess $ shell $ "sh " ++ target ++ ".do - - " ++ tmp ++ " > " ++ tmp
+  exit <- waitForProcess ph
+  case exit of
+    ExitSuccess -> do renameFile tmp target
+    ExitFailure code -> do hPutStrLn stderr $ "Error. Exited with: " ++ show code
+                           removeFile tmp
